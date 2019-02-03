40351f5e188fb9a6107a6344fc463b2659e874a3
Anthony Doan <mythicalprogrammer@gmail.com>
2019-02-02 19:00:08 -0800

    Added a route with nonexisting controller to test out login functionality.

--- lib/fumigate_web/router.ex
+++ lib/fumigate_web/router.ex
@@ -1,5 +1,6 @@
 defmodule FumigateWeb.Router do
   use FumigateWeb, :router
+  use Coherence.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -7,15 +8,41 @@ defmodule FumigateWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session  
   end
+
+
+  pipeline :protected do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session, protected: true
+  end
+
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
+  scope "/" do
+    pipe_through :browser
+    coherence_routes
+  end
+
+  scope "/" do
+    pipe_through :protected
+    coherence_routes :protected
+  end
+
+  scope "/", FumigateWeb do
+    pipe_through :protected 
+    resources "/testlogin", TestController
+  end
+
   scope "/", FumigateWeb do
     pipe_through :browser
-
     get "/", PageController, :index
   end
 
