5f947b26c2c3c7d0e1e835e8a9d230eb2a67a4ea
Anthony Doan <mythicalprogrammer@gmail.com>
2019-02-09 22:26:52 -0800

    generated schema for country and migration file.

---
+++ lib/fumigate/fragrance/country.ex
@@ -0,0 +1,19 @@
+defmodule Fumigate.Fragrance.Country do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "countries" do
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(country, attrs) do
+    country
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> unique_constraint(:name)
+  end
+end
