fed28620c433b8ab77daf09be67fdf90d0c9d2b0
Anthony Doan <mythicalprogrammer@gmail.com>
2019-02-12 18:14:31 -0800

    Made company type field name non nullable.

--- lib/fumigate/fragrance/company_type.ex
+++ lib/fumigate/fragrance/company_type.ex
@@ -4,7 +4,7 @@ defmodule Fumigate.Fragrance.Company_Type do
 
 
   schema "company_types" do
-    field :company_type, :string
+    field :company_type, :string, null: false
 
     many_to_many :companies, Fumigate.Fragrance.Company, join_through: Fumigate.Fragrance.Company_Type_Join
     timestamps()
