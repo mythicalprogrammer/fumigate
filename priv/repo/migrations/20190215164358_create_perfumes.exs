2db0f0f1e17b690f976987122d61685aad4b7f39
Anthony Doan <mythicalprogrammer@gmail.com>
2019-02-16 15:21:13 -0800

    Define down function in perfume migration for custom type gender enum.

--- priv/repo/migrations/20190215164358_create_perfumes.exs
+++ priv/repo/migrations/20190215164358_create_perfumes.exs
@@ -7,7 +7,7 @@ defmodule Fumigate.Repo.Migrations.CreatePerfumes do
       add :perfume_name, :string
       add :concentration, :string
       add :gender, GenderEnum.type() 
-      add :perfume_description, :string
+      add :perfume_description, :text
       add :picture_url, :string
       add :year_released, :integer
       add :month_released, :integer
@@ -16,5 +16,10 @@ defmodule Fumigate.Repo.Migrations.CreatePerfumes do
       timestamps()
     end
 
+  end
+
+  def down do
+    drop table(:perfumes)
+    Ecto.Migration.execute "DROP TYPE gender"
   end
 end
