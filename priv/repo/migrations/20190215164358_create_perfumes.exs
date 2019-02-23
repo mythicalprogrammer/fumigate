d3bc2b1da80512e3a3e0b34dc48e0851815366b5
Anthony Doan <mythicalprogrammer@gmail.com>
2019-02-23 04:56:18 -0800

    Convert month field in perfume into month postgresql enum type.

--- priv/repo/migrations/20190215164358_create_perfumes.exs
+++ priv/repo/migrations/20190215164358_create_perfumes.exs
@@ -10,7 +10,7 @@ defmodule Fumigate.Repo.Migrations.CreatePerfumes do
       add :perfume_description, :text
       add :picture_url, :string
       add :year_released, :integer
-      add :month_released, :integer
+      add :month_released, MonthEnum.type() 
       add :day_released, :integer
 
       timestamps()
@@ -21,5 +21,6 @@ defmodule Fumigate.Repo.Migrations.CreatePerfumes do
   def down do
     drop table(:perfumes)
     Ecto.Migration.execute "DROP TYPE gender"
+    Ecto.Migration.execute "DROP TYPE month"
   end
 end
