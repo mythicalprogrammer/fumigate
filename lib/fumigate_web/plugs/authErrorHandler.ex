cda507ceee12978fcd7d5ebfc095ca47221ad2e0
Anthony Doan <mythicalprogrammer@gmail.com>
2019-03-06 17:10:01 -0800

    Refactor some guardian stuff. Started to remove json api and identity package.

--- lib/fumigate_web/plugs/authErrorHandler.ex
+++ lib/fumigate_web/plugs/authErrorHandler.ex
@@ -1,11 +1,13 @@
 defmodule FumigateWeb.AuthErrorHandler do
   import Plug.Conn
-  import Phoenix.Controller, only: [json: 2]
 
+  @behaviour Guardian.Plug.ErrorHandler
+
+  @impl Guardian.Plug.ErrorHandler
   def auth_error(conn, {type, _reason}, _opts) do
-	conn
-	|> put_status(401)
-	|> json(%{message: to_string(type)})
-	|> halt()
+    body = to_string(type)
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(401, body)
   end
 end
