e6827cb72202817cf7cf6155a8138335c722533a
Anthony Doan <mythicalprogrammer@gmail.com>
2019-03-29 01:15:37 -0700

    refactor code so company and manage company template can use the same view functions.

--- lib/fumigate_web/views/view_helpers.ex
+++ lib/fumigate_web/views/view_helpers.ex
@@ -1,4 +1,10 @@
 defmodule FumigateWeb.ViewHelpers do
   @moduledoc """
   """
+
+  def country_name(%{country: %{name: name}}), do: name
+  def country_name(_), do: "Unknown" 
+
+  def main_activity(%{company_main_activity: %{main_activity: main_activity}}), do: main_activity 
+  def main_activity(_), do: "Unknown" 
 end
