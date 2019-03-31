9b68bbd5a7b1e4eaf275fb0cd25b3100c8a2ecf0
Anthony Doan <mythicalprogrammer@gmail.com>
2019-03-30 21:09:17 -0700

    Navbar links are now admin if admin is login.

--- lib/fumigate_web/views/view_helpers.ex
+++ lib/fumigate_web/views/view_helpers.ex
@@ -3,6 +3,10 @@ defmodule FumigateWeb.ViewHelpers do
   """
 
   def username(%Plug.Conn{assigns: %{current_user: %Fumigate.Users.User{username: username}}}), do: to_string(username)
+  def username(_), do: ""
+
+  def role(%Plug.Conn{assigns: %{current_user: %Fumigate.Users.User{role: role}}}), do: to_string(role)
+  def role(_), do: "" 
 
   def login?(%Plug.Conn{assigns: %{current_user: %Fumigate.Users.User{email: email}}}) when is_binary(email), do: true 
   def login?(_), do: false 
