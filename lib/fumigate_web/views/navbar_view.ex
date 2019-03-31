1ea7c2a9f2f5e8c206a659a3f2191225aee416a7
Anthony Doan <mythicalprogrammer@gmail.com>
2019-03-30 18:34:19 -0700

    Added username function to return username to view helper file.

--- lib/fumigate_web/views/navbar_view.ex
+++ lib/fumigate_web/views/navbar_view.ex
@@ -1,6 +1,3 @@
 defmodule FumigateWeb.NavbarView do
   use FumigateWeb, :view
-
-  def login?(%Plug.Conn{assigns: %{current_user: %Fumigate.Users.User{email: email}}}) when is_binary(email), do: true 
-  def login?(_), do: false 
 end
--- lib/fumigate_web/views/view_helpers.ex
+++ lib/fumigate_web/views/view_helpers.ex
@@ -2,6 +2,11 @@ defmodule FumigateWeb.ViewHelpers do
   @moduledoc """
   """
 
+  def username(%Plug.Conn{assigns: %{current_user: %Fumigate.Users.User{username: username}}}), do: to_string(username)
+
+  def login?(%Plug.Conn{assigns: %{current_user: %Fumigate.Users.User{email: email}}}) when is_binary(email), do: true 
+  def login?(_), do: false 
+
   def country_name(%{country: %{name: name}}), do: name
   def country_name(_), do: "Unknown" 
 
