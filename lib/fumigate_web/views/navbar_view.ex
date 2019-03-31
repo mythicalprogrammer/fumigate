610f70f82409b54c9b5a763ddd2e710885db61f1
Anthony Doan <mythicalprogrammer@gmail.com>
2019-03-30 17:26:44 -0700

    Navbar will not show login page when user are login.

--- lib/fumigate_web/views/navbar_view.ex
+++ lib/fumigate_web/views/navbar_view.ex
@@ -1,3 +1,6 @@
 defmodule FumigateWeb.NavbarView do
   use FumigateWeb, :view
+
+  def login?(%Plug.Conn{assigns: %{current_user: %Fumigate.Users.User{email: email}}}) when is_binary(email), do: true 
+  def login?(_), do: false 
 end
