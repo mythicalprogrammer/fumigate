84120e26c31f4db57b52e7759b7d10a2e66002eb
Anthony Doan <mythicalprogrammer@gmail.com>
2019-04-05 11:59:45 -0700

    Added recaptcha to login page.

--- lib/fumigate_web/controllers/pow/session_controller.ex
+++ lib/fumigate_web/controllers/pow/session_controller.ex
@@ -8,19 +8,14 @@ defmodule FumigateWeb.Pow.SessionController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    conn
-    |> Pow.Plug.authenticate_user(user_params)
-    |> case do
-      {:ok, conn} ->
-        conn
-        |> put_flash(:info, "Welcome back!")
-        |> redirect(to: Routes.page_path(conn, :index))
+    case Recaptcha.verify(conn.params["g-recaptcha-response"]) do
+      {:ok, _response} -> create_session(conn, user_params)
 
-      {:error, conn} ->
+      {:error, _errors} -> 
         changeset = Pow.Plug.change_user(conn, conn.params["user"])
 
         conn
-        |> put_flash(:info, "Invalid email or password")
+        |> put_flash(:error, "There is a problem with Recaptcha.")
         |> render("new.html", changeset: changeset)
     end
   end
@@ -28,5 +23,22 @@ defmodule FumigateWeb.Pow.SessionController do
   def delete(conn, _params) do
     {:ok, conn} = Pow.Plug.clear_authenticated_user(conn)
     redirect(conn, to: Routes.page_path(conn, :index))
+  end
+
+  defp create_session(conn, user_params) do
+    conn
+    |> Pow.Plug.authenticate_user(user_params)
+    |> case do
+      {:ok, conn} ->
+        conn
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, conn} ->
+        changeset = Pow.Plug.change_user(conn, conn.params["user"])
+
+        conn
+        |> put_flash(:error, "Invalid email or password")
+        |> render("new.html", changeset: changeset)
+    end
   end
 end
