8b15285d65c3e985ef56a2e18db8a1e34b11b64f
Anthony Doan <mythicalprogrammer@gmail.com>
2019-04-05 16:49:20 -0700

    created migration file for perfume_approvals table.

---
+++ priv/repo/migrations/20190405234211_create_perfume_approvals_table.exs
@@ -0,0 +1,23 @@
+defmodule Fumigate.Repo.Migrations.CreatePerfumeApprovalsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:perfume_approvals) do
+      add :perfume_name, :string
+      add :concentration, :string
+      add :gender, GenderEnum.type() 
+      add :perfume_description, :text
+      add :picture_url, :string
+      add :year_released, :integer
+      add :month_released, MonthEnum.type() 
+      add :day_released, :integer
+
+      timestamps()
+    end
+
+  end
+
+  def down do
+    drop table(:perfume_approvals)
+  end
+end
