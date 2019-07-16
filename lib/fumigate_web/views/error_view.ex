fa251de1e5eaab138b17a1be888d2eb0e38e4f4f
Anthony Doan <mythicalprogrammer@gmail.com>
2019-07-16 06:57:02 -0700

    Added custom 404 and 500 page error.

--- lib/fumigate_web/views/error_view.ex
+++ lib/fumigate_web/views/error_view.ex
@@ -1,6 +1,5 @@
 defmodule FumigateWeb.ErrorView do
   use FumigateWeb, :view
-
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.html", _assigns) do
