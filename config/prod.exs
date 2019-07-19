13b413ad88941014d70a34b5649ecdd78e4f7396
Anthony Doan <mythicalprogrammer@gmail.com>
2019-07-19 11:55:01 -0700

    edited prod.exs to force SSL connection

--- config/prod.exs
+++ config/prod.exs
@@ -58,8 +58,8 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-#     config :fumigate, FumigateWeb.Endpoint,
-#       force_ssl: [hsts: true]
+config :fumigate, FumigateWeb.Endpoint,
+  force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
