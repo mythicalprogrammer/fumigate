8ccfa21716d1fd3226cd2f68f0b0627c4ec8e748
Anthony Doan <mythicalprogrammer@gmail.com>
2019-07-18 23:40:32 -0700

    Undo the port 80 changed. Need to run on 4000 as deploy user. Cannot run port 80 as non root username. Do not run app as root for port 80. Bad idea. Just route incoming http and https to 4000.

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :fumigate, FumigateWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT")],
+  http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "fumigatedb.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
