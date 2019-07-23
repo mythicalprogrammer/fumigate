24fbcc3bf8188f5470d5d8d3e98cc794c1288e6a
Anthony Doan <mythicalprogrammer@gmail.com>
2019-07-22 18:44:32 -0700

    Finished approval perfume show.

--- lib/fumigate/approval.ex
+++ lib/fumigate/approval.ex
@@ -7,6 +7,8 @@ defmodule Fumigate.Approval do
   alias Fumigate.Repo
 
   alias Fumigate.Approval.PerfumeApproval
+  alias Fumigate.Approval.PerfumeApprovalNoteJoin
+  alias Fumigate.Fragrance.Note
 
   def change_perfume_approval(%PerfumeApproval{} = perfume) do
     PerfumeApproval.changeset(perfume, %{})
@@ -23,4 +25,45 @@ defmodule Fumigate.Approval do
     |> PerfumeApproval.get_all_perfume_approvals_preload() 
     |> Repo.paginate(params)
   end 
+
+  def get_perfume!(id), do: Repo.get!(PerfumeApproval, id)
+
+  def get_all_top_notes_by_perfume_id(id) do
+      top_note_ids = select_all_top_notes_by_perfume_id(id) 
+      Note
+      |> Note.get_all_by_id(top_note_ids) 
+      |> Repo.all()
+  end
+
+  def get_all_middle_notes_by_perfume_id(id) do
+      middle_note_ids = select_all_middle_notes_by_perfume_id(id) 
+      Note
+      |> Note.get_all_by_id(middle_note_ids) 
+      |> Repo.all()
+  end
+
+  def get_all_base_notes_by_perfume_id(id) do
+      base_note_ids = select_all_base_notes_by_perfume_id(id) 
+      Note
+      |> Note.get_all_by_id(base_note_ids) 
+      |> Repo.all()
+  end
+
+  def select_all_top_notes_by_perfume_id(id) do
+    select_all_notes_by_perfume_id(id, "top") 
+  end
+
+  def select_all_middle_notes_by_perfume_id(id) do
+    select_all_notes_by_perfume_id(id, "middle") 
+  end
+
+  def select_all_base_notes_by_perfume_id(id) do
+    select_all_notes_by_perfume_id(id, "base") 
+  end
+
+  defp select_all_notes_by_perfume_id(perfume_id, pyramid_note) do
+    PerfumeApprovalNoteJoin
+    |> PerfumeApprovalNoteJoin.get_all_notes_by_perfume_approval_id(perfume_id, pyramid_note) 
+    |> Repo.all()
+  end
 end
--- lib/fumigate_web/controllers/admin/perfume_approval_controller.ex
+++ lib/fumigate_web/controllers/admin/perfume_approval_controller.ex
@@ -12,4 +12,15 @@ defmodule FumigateWeb.Admin.PerfumeApprovalController do
     page = Approval.list_perfume_approvals_paginate(params) 
     render(conn, "index.html", page: page)
   end
+
+  def show(conn, %{"id" => id}) do
+    perfume = Approval.get_perfume!(id)
+              |> Fumigate.Repo.preload([:accords, :companies])
+    top_notes = Approval.get_all_top_notes_by_perfume_id(id)
+    middle_notes = Approval.get_all_middle_notes_by_perfume_id(id)
+    base_notes = Approval.get_all_base_notes_by_perfume_id(id)
+    #render(conn, "show.html", perfume: perfume)
+           render(conn, "show.html", perfume: perfume,
+           top_notes: top_notes, middle_notes: middle_notes, base_notes: base_notes)
+  end
 end
