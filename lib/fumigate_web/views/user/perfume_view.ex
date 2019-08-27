b8cfe4c344a8cb0e5a3040faaeaa974f0e188e96
Anthony Doan <mythicalprogrammer@gmail.com>
2019-08-26 17:36:10 -0700

    Removed unused function.

--- lib/fumigate_web/views/user/perfume_view.ex
+++ lib/fumigate_web/views/user/perfume_view.ex
@@ -1,15 +1,3 @@
 defmodule FumigateWeb.User.PerfumeView do
   use FumigateWeb, :view
-
-  def note_parse(notes) do
-    notes 
-    |> Enum.map( 
-         fn note -> "<a href='/notes/#{note.id}'>#{note.note_name}</a>" 
-    end)
-    |> Enum.sort
-    |> Enum.join(", ")
-    |> HtmlSanitizeEx.basic_html 
-    |> raw 
-  end
-
 end
